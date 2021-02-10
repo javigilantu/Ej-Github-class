36cb2e3a7e37fbc082dfafa769ac0f2285bbe794
Miguelmvf98 <Miguelmvf@gmail.com>
2021-02-10 13:53:07 +0100

    final template

--- api.py
+++ api.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 @app.route( "/" )
 def landing_page():
     return render_template( 'index.html' )
-if __name__ == '__main__' :
-app.run(host= '0.0.0.0' ,port= 1991 , debug= True )
+
 @app.route( "/dashboard" )
 def dashboard():
     return render_template( 'dashboard.html' )
@@ -13,3 +12,6 @@ def dashboard():
 @app.route( "/map" )
 def map():
     return render_template( 'map.html' )
+
+if __name__ == '__main__' :
+    app.run(host= '0.0.0.0' ,port= 1991 , debug= True )
